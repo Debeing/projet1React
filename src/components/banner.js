ba0de5e44c9a700befeba6cca42d2f20bd766d7a
Debeing <mbaitelameric@gmail.com>
2025-05-27 23:34:45 +0100

    ...

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,6 +1,13 @@
 function Banner() {
     return (<div className="banner">
-                <h1>Bonjour maison jungle</h1>
+                <h1>La maison jungle</h1>
+                <h2>Panier</h2>
+                <ul>
+                    <li>monstera: 8£</li>
+                    <li>Fleurs: 10£</li>
+                    <li>Accessoires: 15£</li>
+                </ul>
+                <p>Total: 33£</p>
              </div>
              );
 }
